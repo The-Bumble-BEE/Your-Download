6d29fc4da1c82b0da4a40a7a07b638583eadc8fb
The-Bumble-BEE <157389374+The-Bumble-BEE@users.noreply.github.com>
2024-02-02 22:33:50 +0100

    Add files via upload
    A first installer for version 0.2 of the Y-Store. If there are any bugs please report them, then they can be removed. It must be ensured that pip and Pyhton work! The program is then located in /BumbleBee which is created

---
+++ Install-Y-Store-V0.2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+echo "Do you want to install Y-Store"
+sudo apt install chromium-browser -y #Is used for creating Webapps
+echo "Installing Chromium"
+echo "DONE!"
+echo "Installing Python3 with TkInter"
+sudo apt-get install python3-tk -y
+echo "DONE!"
+echo "Installing dependences"
+pip install --upgrade pip
+pip install --upgrade pip setuptools
+echo "Installing customtkinter"
+pip install customtkinter 
+echo "requests" 
+pip install requests 
+echo "DONE!"
+echo "Downloading Your-Store"
+echo "Installing"
+cd /
+sudo mkdir "BumbleBee"
+echo "Installing."
+cd BumbleBee
+echo "Installing.."
+sudo wget -O Your-Store-main.zip "https://github.com/The-Bumble-BEE/Your-Store/archive/main.zip"
+echo "Installing..."
+sudo unzip Your-Store-main.zip 'Your-Store-main/Your-Store-V.0.2/*' -d /BumbleBee
+echo "Installing..."
+sudo rm Your-Store-main.zip
+read -p "Installation finished! Press Enter to close this window"
