a8423fb481c5568bad0e5cb38ec36d7e5872d927
The-Bumble-BEE <157389374+The-Bumble-BEE@users.noreply.github.com>
2024-02-08 11:01:11 +0100

    Create Install-Y-Store-V0.3.sh

---
+++ Install-Y-Store-V0.3.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo "Do you want to install Y-Store"
+sudo apt install chromium-browser -y #Is used for creating Webapps
+echo "Installing Chromium"
+echo "DONE!"
+echo "Installing Python3 with TkInter"
+sudo apt-get install python3-tk -y
+echo "DONE!"
+echo "Installing dependences"
+pip install --upgrade pip
+pip install --upgrade pip setuptools
+echo "Installing customtkinter"
+pip install customtkinter 
+echo "requests" 
+pip install requests 
+echo "DONE!"
+echo "Downloading Your-Store"
+echo "Installing"
+cd /
+sudo mkdir "/home/$USER/BumbleBee"
+echo "Installing."
+cd /home/$USER/BumbleBee
+echo "Installing.."
+sudo wget -O Your-Store-main.zip "https://github.com/The-Bumble-BEE/Your-Store/archive/main.zip"
+echo "Installing..."
+sudo unzip Your-Store-main.zip 'Your-Store-main/Your-Store-V.0.3/*' -d /home/$USER/BumbleBee
+echo "Installing..."
+sudo rm Your-Store-main.zip
+cd ..
+echo "Installing...."
+sudo chmod -R 777 BumbleBee
+read -p "Installation finished! Press Enter to close this window"
